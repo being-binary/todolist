b707a5c3bdf31d570a903a0b8c4613f8710d1a15
Abhishek Gupta <cryprincesis007@gmail.com>
2025-01-13 14:34:09 +0530

    tailwind update

--- src/pages/TodoList.jsx
+++ src/pages/TodoList.jsx
@@ -56,13 +56,11 @@ const TodoList = () => {
         setList([...task])
     }
     return (
-
-
-        <div className="relative overflow-x-auto g:px-[10vw] md:px-[10vw] px-[5vw] py-5">
-            <table className="w-full text-sm rtl:text-right text-gray-500 dark:text-gray-400 text-center rounded-md">
+       <div className="container m-auto relative overflow-x-auto py-5">
+            <table cellPadding={10} className="w-full text-sm rtl:text-right text-gray-500 dark:text-gray-400 text-center rounded-md">
                 <thead className="text-xs text-gray-700 uppercase bg-gray-100 dark:bg-gray-700 dark:text-gray-400">
                     <tr>
-                        <th scope="col" className="px-6 py-3">
+                        <th scope="col" className="px-6 py-3 ">
                             task no
                         </th>
                         <th scope="col" className="px-6 py-3">
@@ -71,7 +69,7 @@ const TodoList = () => {
                         <th scope="col" className="px-6 py-3">
                             status
                         </th>
-                        <th scope="col" colSpan={2} className="px-6 py-3">
+                        <th scope="col" className="px-6 py-3">
                             Action
                         </th>
                     </tr>
@@ -79,22 +77,22 @@ const TodoList = () => {
                 <tbody>
                     {
                         list.map((task, index) => {
-                            return <tr key={index} className={` px-6 py-4 text-xl ${task.status == 'pending' ? 'bg-gray-400' : task.status == 'completed' ? 'bg-green-400' : 'bg-yellow-400'}`}>
-                                <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                            return <tr key={index} className={`  text-xl ${task.status == 'pending' ? 'bg-gray-400' : task.status == 'completed' ? 'bg-green-400' : 'bg-yellow-400'}`}>
+                                <th scope="row" className=" font-medium text-gray-900 whitespace-nowrap dark:text-white">
                                     {index + 1}
                                 </th>
-                                <td className="px-6 py-4">
+                                <td className="">
                                     {task.taskName}
                                 </td>
-                                <td className="px-6 py-4 capitalize">
+                                <td className=" capitalize">
                                     {task.status}
                                 </td>
-                                <td className="px-6 py-4 flex justify-center grid-cols-2 gap-2">
-                                    <button className='px-2 py-3 bg-red-400 rounded-md capitalize text-white hover:bg-red-600 w-[120px] h-[50px]' onClick={() => deleteTask(index)}>delete</button>
-                            {task.status!=='completed' && <button className={` ${hide && 'hidden'} 'px-2 py-3 bg-red-400 rounded-md capitalize text-white hover:bg-red-600 w-[120px] h-[50px]'`} onClick={() => updateTask(index)}>
-                                        {
-                                            task.status == "pending" ? 'Start' : task.status == 'completed' ? 'Finished' : 'complete'
-                                        }
+                                <td className=" grid justify-center md:grid-cols-2 grid-cols-1 gap-5 ">
+                                    <button className=' bg-red-400 rounded-md capitalize text-white hover:bg-red-600 w-[120px] h-[50px]' onClick={() => deleteTask(index)}>delete</button>
+                                    {task.status!=='completed' && <button className={` ${hide && 'hidden'} 'px-2 py-3 bg-red-400 rounded-md capitalize text-white hover:bg-red-600 w-[120px] h-[50px]'`} onClick={() => updateTask(index)}>
+                                                {
+                                                    task.status == "pending" ? 'Start' : task.status == 'completed' ? 'Finished' : 'complete'
+                                                }
                                     </button>}
                                 </td>
                             </tr>
